3f2ed3dfe7abc51290b21967a3de4679ea75219b
Денис <1ki15b.kuchmei@gmail.com>
2019-05-28 12:14:05 +0300

    Initial commit

---
+++ src/ArrayPairSum.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ArrayPairSum {
+    static int [] array = {10,12,13,4,15,9,45,74,52};
+
+    public static void main(String[] args) {
+   findAndPrintPairs(array, 25);
+    }
+
+    static List<Integer> findAndPrintPairs(int[]input, int sum) {
+        List<Integer> pairs  = new ArrayList<>();
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                if (input[i] + input[j] == sum) {
+                pairs.add(input[i]);
+                pairs.add(input[j]);
+                System.out.print(input[i] +" " + input[j] + ", " );
+                }
+            }
+        }
+       return pairs;
+    }
+}
